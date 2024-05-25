4f6ee9bd0379ac4e3a288e68ddb0cc076a548192
sunnykurmi <shivanibillore22@gmail.com>
2024-05-25 14:40:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// src/App.jsx
+import "./App.css";
+import StopWatch from "./components/StopWatch";
+import Timer from "./components/Timer";
+import { useState } from "react";
+
+function App() {
+  const [showTimer, setShowTimer] = useState(true);
+  const [showStopWatch, setShowStopWatch] = useState(true);
+
+  return (
+    <div className="App">
+      {showTimer ? <Timer /> : null}
+      <button onClick={() => setShowTimer(!showTimer)}>TOGGLE TIMER</button>
+      <br />
+      <br />
+      <hr />
+      {showStopWatch && <StopWatch />}
+      <button onClick={() => setShowStopWatch(!showStopWatch)}>
+        TOGGLE STOPWATCH
+      </button>
+    </div>
+  );
+}
+
+export default App;
